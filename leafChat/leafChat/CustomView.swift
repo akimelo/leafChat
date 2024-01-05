7880cf82179c332216b1f350c611b668a39a2dc4
akiJacky <tyatya38an@gmail.com>
2024-01-05 19:42:19 +0900

    add ChatView

--- leafChat/leafChat/CustomView.swift
+++ leafChat/leafChat/CustomView.swift
@@ -8,6 +8,29 @@
 import Foundation
 import UIKit
 
+class InsetLabel: UILabel {
+    var contentInsets = UIEdgeInsets.init(top: 0, left: 10, bottom: 0, right: 10)
+    
+    override func drawText(in rect: CGRect) {
+        let insetRect = rect.inset(by: contentInsets)
+        super.drawText(in: insetRect)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return addInsets(to: super.intrinsicContentSize)
+    }
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return addInsets(to: super.sizeThatFits(size))
+    }
+    
+    private func addInsets(to size: CGSize) -> CGSize {
+        let width = size.width + contentInsets.left + contentInsets.right
+        let height = size.height + contentInsets.top + contentInsets.bottom
+        return CGSize(width: width, height: height)
+    }
+}
+
 extension UIImage {
     func resize(toWidth width: CGFloat) -> UIImage? {
         let resizedSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
