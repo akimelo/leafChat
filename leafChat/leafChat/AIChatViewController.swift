99296ba29caf1705bf8ac282f86014cbd5196e80
akiJacky <tyatya38an@gmail.com>
2024-01-06 20:49:29 +0900

    add AIChatViewController

---
+++ leafChat/leafChat/AIChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AIChatViewController.swift
+//  leafChat
+//
+//  Created by Akihiro Nakano on 2024/01/06.
+//
+
+import UIKit
+
+class AIChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
