b7b3d8dd0d2e22a7a77e99e66b003fc9cd8af2d7
akiJacky <tyatya38an@gmail.com>
2024-01-11 23:53:37 +0900

    modify ViewController

--- leafChat/leafChat/ViewController.swift
+++ leafChat/leafChat/ViewController.swift
@@ -1,87 +1,106 @@
 //
-//  ViewController.swift
+//  MenuController.swift
 //  leafChat
 //
-//  Created by Akihiro Nakano on 2024/01/01.
+//  Created by Akihiro Nakano on 2024/01/06.
 //
 
 import UIKit
 import KarteCore
+import Nativebrik
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+let env = try! LoadEnv()
+let nativebrik = {
+    return NativebrikClient(projectId: env.value("PROJECT_ID_NATIVEBRIK")!)
+}()
 
-    @IBOutlet weak var tableView: UITableView!
+class ViewController: UIViewController {
     
-    var dataHelper = DatabaseHelper()
-    var roomList:[ChatRoom] = []
+//    private let nbView = NativeBrikView()
+    @IBOutlet weak var iconVIew: UIImageView!
+    @IBOutlet weak var iconName: UILabel!
     
+    override func loadView() {
+        super.loadView()
+        let uiview = nativebrik
+            .experiment
+            .embeddingUIView("TEST_1")
+        uiview.frame = CGRect(x: 20, y: 635, width: UIScreen.main.bounds.width - 40, height: 65)
+        self.view.addSubview(uiview)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // add nativebrik.overlay to the top.
+        iconVIew.layer.cornerRadius = iconVIew.frame.size.width * 0.5
+        
+        let overlay = nativebrik.overlayViewController()
+        self.addChild(overlay)
+        self.view.addSubview(overlay.view)
+
+        // Do any additional setup after loading the view.        
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        let window = windowScene?.windows.first
+        window?.overrideUserInterfaceStyle = .dark
+        
         let uid = AuthHelper().uid()
+        let database = DatabaseHelper()
+        
         if uid == "" {
             performSegue(withIdentifier: "login", sender: nil)
         } else {
-            print(uid)
-            //チャットリストを表示する処理
-            dataHelper.getMyRoomList(result: {
-                result in
-                print(result)
-                self.roomList = result
-                self.tableView.reloadData()
+            var iconimageurl = ""
+            let email = AuthHelper().email()
+            
+            database.getImage(userID: uid, imageView: iconVIew)
+            database.getUserName(userID: uid, result: {
+                name in
+                self.iconName.text = name
             })
+            
+            database.getImageURL(userID: uid) { imageUrl, error in
+                if let error = error {
+                    // エラー処理
+                    print("Error getting image URL: \(error)")
+                } else if let imageUrl = imageUrl {
+                    // imageUrlを使用した処理
+                    iconimageurl = imageUrl
+                    print("Download URL: \(imageUrl)")
+                    Tracker.track("attribute", values: [
+                        "icon_img": iconimageurl,
+                        "email": email
+                    ])
+                }
+            }
+            
+            database.getUserName(userID: uid, result: {
+                result in
+                if result == ""{
+                    print("Can't find user_id")
+                } else {
+                    print("imageurl: " + iconimageurl)
+                    print("KARTE login")
+                    Tracker.identify(
+                        uid,
+                        ["name": result]
+                    )
+                }
+            })
+            print(uid)
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        Tracker.view("chat_list", title: "チャット一覧")
-//        print("KARTE_chat_list")
+        Tracker.view("menu", title: "メニュー")
+//        print("KARTE_menu")
     }
-
+    
     @IBAction func onLogOut(_ sender: Any) {
         AuthHelper().signout()
         performSegue(withIdentifier: "login", sender: nil)
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("numberOfRowsInSection")
-        print(roomList.count)
-        return roomList.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        print("heightForRowAt")
-        return 80.0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellData = roomList[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        let imageView = cell?.viewWithTag(1) as! UIImageView
-        imageView.layer.cornerRadius = imageView.frame.size.width * 0.5
-        imageView.clipsToBounds = true
-        print("userID:" + cellData.userID)
-        dataHelper.getImage(userID: cellData.userID, imageView: imageView)
-        let nameLabel = cell?.viewWithTag(2) as! UILabel
-        dataHelper.getUserName(userID: cellData.userID, result: {
-            name in
-            nameLabel.text = name
-            print("name:" + name)
-        })
-        return cell!
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        performSegue(withIdentifier: "chat", sender: roomList[indexPath.row])
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "chat"{
-            let VC = segue.destination as! ChatView
-            let data = sender as! ChatRoom
-            VC.roomData = data
-        }
-    }
-}
 
+}
