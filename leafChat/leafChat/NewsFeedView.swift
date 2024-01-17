797031cce31ad83c834e4cf73398439e468aea39
akiJacky <tyatya38an@gmail.com>
2024-01-17 23:58:41 +0900

    add NewsFeedView

---
+++ leafChat/leafChat/NewsFeedView.swift
@@ -0,0 +1,29 @@
+//
+//  NewsFeedView.swift
+//  leafChat
+//
+//  Created by Akihiro Nakano on 2024/01/17.
+//
+
+import UIKit
+
+class NewsFeedView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
