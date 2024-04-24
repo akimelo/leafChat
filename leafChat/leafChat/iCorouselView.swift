68317d62ea0290caf1acf7e1ef22f95c62cfbdb3
akiJacky <tyatya38an@gmail.com>
2024-04-24 18:28:00 +0900

    add iCorouselView

---
+++ leafChat/leafChat/iCorouselView.swift
@@ -0,0 +1,31 @@
+//
+//  iCorouselView.swift
+//  leafChat
+//
+//  Created by Akihiro Nakano on 2024/04/23.
+//
+
+import UIKit
+import KarteCore
+import KarteVariables
+
+class iCorouselView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
